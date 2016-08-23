d677f2046b6256fb3f7b8690453b02d21f69dcc6
Luo Yi <luoyi.ly@gmail.com>
2016-08-23 14:54:18 +0800

    support multi def in one library

--- kilib/kilib.py
+++ kilib/kilib.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python2
 from datetime import datetime
+import json
+import sys
 
 
 
 class Header:
     @staticmethod
-    def toString():
+    def HeadString():
         return """EESchema-LIBRARY Version 2.3 Date: %s
 #encoding utf-8
 """ % datetime.now().strftime("%d/%m/%Y %X")
 
+    @staticmethod
+    def TailString():
+        return "#End Library\n"
 
-class Symbols:
-    def __init__(self, name, ref="U", pinoff = 40, show_pnum = 'Y', show_pname = 'Y', parts = 1,  locked = 'F', isPower = 'N' ):
-        self.name = name
-        self.ref  = ref
-        self.pinoff = pinoff
-        self.show_pnum = show_pnum
-        self.show_pname = show_pname
-        self.parts = parts
-        self.locked = locked
-        self.isPower = isPower
+
+class Symbol:
+    def __init__(self, prop_dict):
+        self.name = prop_dict["name"]
+        self.ref  =  prop_dict["ref"] if ("ref" in prop_dict) else "U"
+        self.pinoff  =  prop_dict["pinoff"] if ("pinoff" in prop_dict) else 40
+        self.show_pnum  =  prop_dict["show_pnum"] if ("show_pnum" in prop_dict) else "Y"
+        self.show_pname  =  prop_dict["show_pname"] if ("show_pname" in prop_dict) else "Y"
+        self.parts  =  prop_dict["parts"] if ("parts" in prop_dict) else 1
+        self.locked  =  prop_dict["locked"] if ("locked" in prop_dict) else "F"
+        self.isPower  =  prop_dict["isPower"] if ("isPower" in prop_dict) else "N"
         self.setDefault()
 
     def setDefault(self):
@@ -70,7 +76,15 @@ F3 "" 0 0 50 H I C C
             p = self.pin_obj.getCoord(i+1)
             s += """\
 X %s %d %d %d %d %s 50 50 %d %d %s
-""" % ( self.pins[i][0], i, p[0], p[1], p[2], p[3], 50, 50, self.pins[i][1] )
+""" % ( self.pins[i][0], i, p[0], p[1], p[2], p[3], 1, 1, self.pins[i][1] )
+        return s
+
+    def AllString(self):
+        s = self.DEFString()
+        s += self.FNString()
+        s += "DRAW\n"
+        s += self.PINString()
+        s += "ENDDRAW\nENDDEF\n"
         return s
 
 class DIPPin():
@@ -86,11 +100,11 @@ class DIPPin():
     def getCoord(self, nth):
         if nth <= self.base :
             y = self.base - nth
-            x = 1
+            x = -1
             o = 'R'
         else :
             y = nth - self.base - 1
-            x = -1
+            x = 1
             o = 'L'
         return [self.width/2 * x, self.base_y + self.hgap * y, self.size, o]
 
@@ -99,14 +113,19 @@ class DIPPin():
         y = abs(self.base_y)  + 200
         return [-x, -y, x, y]
 
+def SymFromJson(jstr):
+    js = json.loads(jstr)
+    rs  = []
+    for j in js:
+        s = Symbol(j)
+        s.setPins(j["pins"])
+        rs.append(s)
+    return rs
 
 
 if __name__ == "__main__":
-    print Header.toString()
-    s = Symbols("ABC")
-    print s.DEFString()
-    print s.FNString()
-    pins = [['VIN', 'W'], ['PPR', 'O'], ['CHG', 'O'], ['EN', 'I'], ['GND', 'W'], ['FAST', 'O'], ['ISET', 'O'], ['BAT', 'O']]
-    s.setPins(pins)
-    print s.PINString()
+    print Header.HeadString()
+    for s in SymFromJson(sys.stdin.read()):
+        print s.AllString()
+    print Header.TailString()
 
